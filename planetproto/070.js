dd717425db6991289eefb3679016ef8cb4c081f5
Daniel Pereira <pereiradpg@yahoo.ca>
2015-09-27 19:24:59 -0400

    Planetproto 070 passed.

---
+++ planetproto/070.js
@@ -0,0 +1,22 @@
+// -> Define an object called 'Robot'
+// -> Define a method called 'new' in Robot
+// -> When Robot.new is called it should return a new object with Robot as its prototype 
+//    e.g. var robby = Robot.new();
+//    Robot should be the prototype of robby
+
+var Robot = {
+	new: function() {
+		return Object.create(this);
+	}
+};
+
+Robot.new();
+
+var robby = Robot.new();
+
+
+// ------------------------------------------------
+// Common JS exports for verification, don't modify
+module.exports = {
+	Robot: Robot
+}
