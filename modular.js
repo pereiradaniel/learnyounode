017c052c08eff4559522a639ee6faaeddda52cce
Daniel Pereira <pereiradpg@yahoo.ca>
2016-11-28 17:21:21 -0500

    Updated notes

---
+++ modular.js
@@ -0,0 +1,23 @@
+// Load function from module.js to variable called myModule
+	var myModule = require('./module.js')
+
+// Load 3rd argument from command line into variable dir
+	var dir = process.argv[2]
+
+// Load 4th argument from command line into variable filterString
+	var filterString = process.argv[3]
+
+// Run myModule passing it the required info
+//		Function takes:
+//			1st argument is directory name
+//			2nd argument is string to filter
+//			3rd argument is a callback function
+	myModule(dir, filterString, function(err, list) {
+		// Print error to console if error
+		if (err)
+			return console.error('Error: ', err)
+		// iterate through list and output each file to the console
+		list.forEach(function (file) {
+			console.log(file)
+		})
+	})
---
+++ module.js
@@ -0,0 +1,19 @@
+// Load filesystem module into fs variable
+	var fs = require('fs')
+// Load path module into path variable
+	var path = require('path')
+// Define a single function export assign it to the module.exports object
+	module.exports = function(dir, fileString, callback) {
+		// use .readdir() to return the dir contens
+		// pass a callback function as third argument
+		fs.readdir(dir, function (err, list) {
+			// return err if error
+			if (err)
+				return callback(err)
+			// use .filter() to return filtered directory contents to list variable
+			list = list.filter(function (file) {
+				return path.extname(file) === '.' + fileString
+			})
+			callback(null, list)
+		})
+	}
