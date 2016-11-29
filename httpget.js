0bb343a2a80da67d2463ae27ffc2a7caf6587cde
Daniel Pereira <pereiradpg@yahoo.ca>
2016-11-29 16:06:49 -0500

    HTTPget passed

---
+++ httpget.js
@@ -0,0 +1,24 @@
+// Load node http module into http variable
+	var http = require('http');
+
+// Load url from first command line argument into url variable
+	var url = process.argv[2].toString();
+
+// Load Buffer List module into bl variable
+	var bl = require('bl');
+
+	http.get(url, function(response) {
+		// Pipe data from response
+			response.pipe(bl(function (err, data) {
+				// Repsond to error
+					if (err) {
+						return console.error(err)
+					}
+				// Else respond to data
+					data = data.toString()
+					// Output number of characters in data string
+					console.log(data.length)
+					// Output complete string of characters from server
+					console.log(data)
+			}))
+	});
