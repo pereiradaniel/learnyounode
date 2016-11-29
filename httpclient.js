2381b02e48b7871da4c9b7283840c54d85b88aa6
Daniel Pereira <pereiradpg@yahoo.ca>
2016-11-29 15:51:50 -0500

    HTTP Client passed

---
+++ httpclient.js
@@ -0,0 +1,25 @@
+//  Load node http module into http variable
+	var http = require('http');
+
+// Load URL from 1st command line arg into url variable
+	var url = process.argv[2].toString();
+
+// Use http.get() method to pass URL and act on response to callback
+	http.get(url, function(response) {
+		// Convert buffer object to string
+			response.setEncoding('utf8')
+		// Respond to data
+			response.on('data', console.log);
+		// Respond to error
+			response.on('error', console.error);
+	}).on('error', console.error);
+
+// Official solution
+
+   // var http = require('http')
+
+   //  http.get(process.argv[2], function (response) {
+   //    response.setEncoding('utf8')
+   //    response.on('data', console.log)
+   //    response.on('error', console.error)
+   //  }).on('error', console.error)
--- modular.js
+++ modular.js
@@ -21,3 +21,19 @@
 			console.log(file)
 		})
 	})
+
+// Official solution:
+
+    // var filterFn = require('./solution_filter.js')
+    // var dir = process.argv[2]
+    // var filterStr = process.argv[3]
+
+    // filterFn(dir, filterStr, function (err, list) {
+    //   if (err) {
+    //     return console.error('There was an error:', err)
+    //   }
+
+    //   list.forEach(function (file) {
+    //     console.log(file)
+    //   })
+    // })
