6568197b11e165d1ec9512341368551001856b08
Daniel <truenorthtrainingcompany@gmail.com>
2017-01-11 16:33:57 -0500

    Added official solution to asyncio.js

--- asyncio.js
+++ asyncio.js
@@ -11,12 +11,23 @@
 	// 	console.log(content.toString().split('\n').length - 1);
 	// });
 
-// Remember that idiomatic Node.js callbacks normally have the signature:
-//		function callback(err, data) { /* ... */ }
-// If no err, second argument should be buffer object
-// 'utf8' can be supplied as the second argument and callback as the third and you will get a string instead of a buffer
+  // Remember that idiomatic Node.js callbacks normally have the signature:
+  //		function callback(err, data) { /* ... */ }
+
+  // If no err, second argument should be buffer object
+  // 'utf8' can be supplied as the second argument and callback as the third and you will get a string instead of a buffer
 
 // SOLUTION 2
 	output = fs.readFile(file, 'utf8', function(err, content){
 		console.log(content.split('\n').length - 1);
 	});
+
+// OFFICIAL SOLUTION
+  // var fs = require('fs')  
+  // var file = process.argv[2]  
+   
+  // fs.readFile(file, function (err, contents) {  
+  //  // fs.readFile(file, 'utf8', callback) can also be used  
+  //  var lines = contents.toString().split('\n').length - 1  
+  //  console.log(lines)  
+  // }) 
