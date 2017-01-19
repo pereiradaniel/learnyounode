9bc255dc9625b065b3de74e6bceb275d9d85716e
Daniel <truenorthtrainingcompany@gmail.com>
2017-01-19 15:39:43 -0500

    Remove unecessary spaces

--- httpclient.js
+++ httpclient.js
@@ -15,7 +15,6 @@
 	}).on('error', console.error);
 
 // Official solution
-
    // var http = require('http')
 
    //  http.get(process.argv[2], function (response) {
--- httpcollect.js
+++ httpcollect.js
@@ -26,16 +26,14 @@
 
 
 // Official solution
-
-	// var http = require('http')  
-	// var bl = require('bl')  
-	 
-	// http.get(process.argv[2], function (response) {  
-	//  response.pipe(bl(function (err, data) {  
-	//    if (err)  
-	//      return console.error(err)  
-	//    data = data.toString()  
-	//    console.log(data.length)  
-	//    console.log(data)  
-	//  }))    
-	// })  
+	// var http = require('http')
+	// var bl = require('bl')
+	// http.get(process.argv[2], function (response) {
+	//  response.pipe(bl(function (err, data) {
+	//    if (err)
+	//      return console.error(err)
+	//    data = data.toString()
+	//    console.log(data.length)
+	//    console.log(data)
+	//  }))
+	// })
