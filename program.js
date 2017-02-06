54b24b35f1d5241168e1ae3bd6bdd66d2516f8ff
Daniel <truenorthtrainingcompany@gmail.com>
2017-02-06 16:33:19 -0500

    Add comment

--- program.js
+++ program.js
@@ -1,4 +1,5 @@
-console.log("HELLO WORLD")
+// Use console.log to output text to the console
+  console.log("HELLO WORLD")
 
 //  Read my blog post for an explanation and breakdown of this challenge:
 //  https://medium.com/coding-and-web-development/learnyounode-exercise-1-907e214c4f28#.pxzcl7vkt
